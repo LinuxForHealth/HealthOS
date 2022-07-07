295dcee9e8fa873f44b114e71756ec63b97e5438
Dixon Whitmire <dixonwh@gmail.com>
2022-07-07 15:41:56 -0400

    Integrated logging with core service
    Signed-off-by: Dixon Whitmire <dixonwh@gmail.com>

--- core/src/linuxforhealth/healthos/core/cli/main.py
+++ core/src/linuxforhealth/healthos/core/cli/main.py
@@ -1,5 +1,10 @@
 import argparse
 from ..config import load_core_configuration, CoreServiceConfig
+from typing import List
+import logging
+import logging.config
+import yaml
+import sys
 
 CLI_DESCRIPTION = """
 The LinuxForHealth HealthOS Core CLI starts Core OS services including:
@@ -9,31 +14,63 @@ The LinuxForHealth HealthOS Core CLI starts Core OS services including:
 - observability
 """
 
+logger = logging.getLogger(__name__)
+
 
 def _start_core_services(args):
     core_config: CoreServiceConfig = load_core_configuration(args.f)
-    for c in core_config.connectors:
-        pass
+
+    # configure logging
+    with open(core_config.logging_config, "r") as f:
+        logging_config = yaml.load(f, Loader=yaml.FullLoader)
+    logging.config.dictConfig(logging_config)
+
+    logger.info("Starting core services")
+
+
+def main(received_arguments: List[str] = None):
+    """
+    Executes the CLI utilities
+    :param received_arguments: The arguments from the CLI. Defaults to None
+    """
+    parser = create_arg_parser()
+    args = parser.parse_args(received_arguments)
+
+    if hasattr(args, "func"):
+        # execute CLI
+        args.func(args)
+    else:
+        parser.print_help()
 
 
 def create_arg_parser():
     arg_parser = argparse.ArgumentParser(
         prog="LFH HealthOS Core",
         description=CLI_DESCRIPTION,
+        formatter_class=argparse.RawTextHelpFormatter
     )
 
     sub_parsers = arg_parser.add_subparsers()
 
     # core
-    core = sub_parsers.add_parser("core", help="starts LFH HealthOS Core Services")
+    core = sub_parsers.add_parser("core", help="starts the LFH HealthOS Core Service using the core configuration file.")
     core.add_argument("-f", help="The path to the core configuration file.")
     core.set_defaults(func=_start_core_services)
 
     # admin
-    admin = sub_parsers.add_parser("admin")
+    admin = sub_parsers.add_parser("admin", help="provides access to running tasks and services.")
     admin_operations = admin.add_mutually_exclusive_group()
     admin_operations.add_argument("-l", help="Lists core service tasks")
     admin_operations.add_argument("-r", help="Restarts a core service task")
     admin_operations.add_argument("-s", help="Stops a core service task")
 
     return arg_parser
+
+
+if __name__ == "__main__":
+    # parsing system arguments and passing them in so that we can:
+    # - bootstrap test cases easily
+    # - print help information if no arguments are provided
+    system_arguments = sys.argv[1:] if sys.argv else []
+    main(system_arguments)
+
