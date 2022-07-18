0d82c7bc72466a376837327103dc17c2d9ad9a86
Dixon Whitmire <dixonwh@gmail.com>
2022-07-18 11:18:58 -0400

    - updated Kafka listener interfaces to support external and internal connections - added send-demo-message.sh to resources/external-kafka to support sending a sample transaction
    Signed-off-by: Dixon Whitmire <dixonwh@gmail.com>

---
+++ core/resources/external-kafka/send-demo-message.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# kafka-send-demo-message.sh
+# sends the sample 270.x12 message to the "healthy-data" topic
+
+/opt/bitnami/kafka/bin/kafka-console-producer.sh \
+    --bootstrap-server localhost:9093 \
+    --topic healthy-data < /opt/scripts/270.x12
